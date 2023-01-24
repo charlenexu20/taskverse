a24fa0fc09d925139ce56f9424664710576ffa5c
charlene <charlene.xu92@gmail.com>
2023-01-23 16:53:46 -0800

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -23,6 +23,7 @@ def redirect_to_home(request):
 
 
 urlpatterns = [
+    path("accounts/", include("accounts.urls")),
     path("", redirect_to_home, name="home"),
     path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
